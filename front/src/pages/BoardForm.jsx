6f708f27c075802d40eea75121a1d9621887c01c
wang_si_eun <alo201011@gmail.com>
2025-12-18 18:15:42 +0900

    [Feat] 수정페이지 내용 수정

--- front/src/pages/BoardForm.jsx
+++ front/src/pages/BoardForm.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { getDetail } from '../service/bbsService';
 import { Box, TextField } from '@mui/material';
 
 const BoardForm = () => {
     const { pstSn } = useParams();
-    const { detail, setDetail } = useState();
+    const [ detail, setDetail ] = useState(null);   // {}안에 내용을 담은 setDetail에 내용이 안담겨.. 왜냐면 useState는 배열을 돌려주지, 객체를 돌려주는게 아님
     console.log("pstSn : ",pstSn);
 
     useEffect(() => {
@@ -12,23 +13,30 @@ const BoardForm = () => {
             try{
                 const data = await getDetail(pstSn);
                 console.log("data : ",data);
-
+                setDetail(data);
             } catch(error){
-                console.log(error);
+                console.log('Error!!',error);
             }
         }
-
-    })
+        if(pstSn) fetch();
+    },[pstSn]); // ,[pstSn] 안 넣으면 계ㅔㅔㅔㅔㅔㅔ속 내용을 부른다.
+    console.log("detail : ",detail);
     return(
         <>
             {/* 상세에서 쓰기 페이지로 넘어올 경우 해당하는 정보들을 물고 들어와야함 */}
             <br/>test form
-            {/* <Box sx={{ width:500, maxWidth: 100% }}>
-                <TextField fullWidth label="fullWidth" id="fullWidth" />
+            <Box sx={{ width:500 }}>
+                <TextField fullWidth id="fullWidth" value={detail?.ttl || ''}/>
             </Box>
-            <Box sx={{ width:500, maxWidth: 100% }}>
-                <TextField fullWidth label="Multiline" id="Multiline" multiline rows={10} defaultValue="내용을 입력하세요." />
-            </Box> */}
+            <Box sx={{ width:500 }}>
+                <TextField
+                    fullWidth id="Multiline"
+                    multiline
+                    rows={10}
+                    defaultValue="내용을 입력하세요."
+                    value={detail?.cn || ''}
+                />
+            </Box>
         </>
     );
 }
